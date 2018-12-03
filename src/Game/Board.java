0dcf5317e6fcbd1472c6bc292072610d463135e3
BT_1N3_29 <jtan5662@bths.edu>
2018-12-03 14:45:33 -0500

    Game board added :)

---
+++ src/Game/Board.java
@@ -0,0 +1,17 @@
+package Game;
+
+import Rooms.Room;
+
+public class Board {
+    private Room[][] map;
+    private int length;
+    private int width;
+
+
+    public Board(int length, int width, Room[][] map) {
+        this.length = length;
+        this.width = width;
+        this.map = map;
+    }
+}
+
